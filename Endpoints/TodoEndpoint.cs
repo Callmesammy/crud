2b6ae93cfa83ee6043947a988f101396e7a13af8
Callmesammy <callmesammy@hayoo.com>
2025-12-11 15:34:22 +0100

    save

--- Endpoints/TodoEndpoint.cs
+++ Endpoints/TodoEndpoint.cs
@@ -2,16 +2,27 @@ namespace Crud.Endpoints;
 
 using Crud.data;
 using Crud.model;
+using Microsoft.EntityFrameworkCore;
 
 public static class ProfileEndpoints
 {
  public static void MapProfileEndpoints (this IEndpointRouteBuilder app) {
- var group = app.MapGroup("/api/profile");
+ var group = app.MapGroup("/api/profiles");
  group.MapPost("/profile", async (ApplicationDbContext db, Profile profile) =>
  {
   db.Profiles.Add(profile);
   await db.SaveChangesAsync();
-  return Results.Created($"/api/profile/{profile.Id}", profile);   
+  return Results.Created($"/api/profiles/{profile.Id}", profile);   
+ });
+group.MapGet("/", async(ApplicationDbContext db) =>
+{
+    await db.Profiles.OrderByDescending(p => p.CreatedAt).ToListAsync();
+});
+
+ group.MapGet("/{id:int}", async (ApplicationDbContext db, int id) =>
+ {
+     var profile = await db.Profiles.FindAsync(id);
+     return profile is null ? Results.NotFound() : Results.Ok(profile);
  });
 
     }
